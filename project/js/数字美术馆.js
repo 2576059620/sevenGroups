7d5332bc820ede2fc48f6533b20e462364df9932
ln941106 <840988292@qq.com>
2017-07-11 14:43:50 +0800

    ln

--- project/js/数字美术馆.js
+++ project/js/数字美术馆.js
@@ -4,4 +4,26 @@ app.controller('goBack',function(){
 	$scope.getBack=function(){
 		window.history.back();
 	}
-})
+});
+
+	$("#gallery").animate({
+		"padding-top":"30px",
+		"opacity":"1"
+	},600);
+	$("#YWART").animate({
+		"padding-top":"20px",
+		"opacity":"1"
+	},1200);
+	$("#not").animate({
+		"padding-top":"20px",
+		"opacity":"1"
+	},1800);
+	$("#unl").animate({
+		"padding-top":"25px",
+		"opacity":"1"
+	},2200);
+	$("#entrance").animate({
+		"margin-top":"-5px",
+		"opacity":"1"
+	},2800);
+
