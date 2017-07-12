038dd745b1eba0c436e21baca2e4c5b98ebad1d8
2576059620 <2576059620@qq.com>
2017-07-12 11:27:42 +0800

    zc

--- project/js/上下.js
+++ project/js/上下.js
@@ -1,33 +1,39 @@
-//上部点击事件
-$(".ys_img1").click(function(){
-			$('.ys_input1').css('display', 'none');
-			$(".ys_input2").css("display","none");
-			$(".ys_input3").css("display","inline");
-			$(".ys_img1").css("display","none");
-			$(".ys_imgx").css("display","inline");
-			$(".ys_a").css("display","none");
-		})
-		$(".ys_imgx").click(function(){
-			$('.ys_input1').css('display', 'inline');
-			$(".ys_input2").css("display","inline");
-			$(".ys_input3").css("display","none");
-			$(".ys_img1").css("display","inline");
-			$(".ys_imgx").css("display","none");
-			$(".ys_a").css("display","inline");
-		})	
-	
+		//上部点击事件
+		$(document).ready(function(){
+			$(".ys_img1").click(function(){
+				$('.ys_input1').css('display', 'none');
+				$(".ys_input2").css("display","none");
+				$(".ys_input3").css("display","inline");
+				// $(".ys_img1").attr('src',"X.jpg"); 
+				$(".ys_img1").css("display","none");
+				$(".ys_imgx").css("display","inline");
+				$(".ys_a").css("display","none");
+				
+				
+			})
+			$(".ys_imgx").click(function(){
+				$('.ys_input1').css('display', 'inline');
+				$(".ys_input2").css("display","inline");
+				$(".ys_input3").css("display","none");
+				// $(".ys_img1").attr('src',"X.jpg"); 
+				$(".ys_img1").css("display","inline");
+				$(".ys_imgx").css("display","none");
+				$(".ys_a").css("display","inline");
+				
+				
+			})
+		}); 
 		//底部点击事件
-		$(document).ready(function(){
- 		 	$(".menu").click(function(){
- 		 		$(".menu").css("display","none");
-				$(".menu1").css("display","inline");
-   		 		$(".ys_footer2").animate({bottom:'10%'});
- 			 });
-		});
-		$(document).ready(function(){
- 		 	$(".menu1").click(function(){
- 		 		$(".menu1").css("display","none");
-				$(".menu").css("display","inline");
-   		 		$(".ys_footer2").animate({bottom:'-35%'});
-  			});
-		});
+		$(document).ready(function () {
+			$(".menu").click(function(){
+				var menu = $(".menu_span2").text()
+				if (menu == "▲") {
+					$(".ys_footer2").animate({"bottom":'50px',"z-index":"-1000"});
+					$(".menu_span2").text("▼")
+				}
+				if (menu == "▼") {
+					$(".ys_footer2").animate({"bottom":'-300px',"z-index":"-1000"});
+					$(".menu_span2").text("▲")
+				}
+			})
+		})
