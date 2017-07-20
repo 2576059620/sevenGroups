3dab67d10b9220095cfba0d8e1d47ca48093eadd
2576059620 <2576059620@qq.com>
2017-07-20 17:40:25 +0800

    zc

--- project/js/造艺网.js
+++ project/js/造艺网.js
@@ -38,7 +38,7 @@
 		})
 
 		// 滑动
-		var swiper = new Swiper('.swiper-container');
+		// var swiper = new Swiper('.swiper-container');
 				// 点击三个图片事件
 		$(document).ready(function(){
 			$(".worke1").click(function(){
@@ -73,65 +73,3 @@
         	$('.frame').animate({opacity:1},500);
 			});		
     }, 3000);
-
-		// footer底部
-		//sectiond底部footer点击事件
-function help1() {
-	var help = $(".help1 span").text();
-	if (help =="+") {
-		$(".display1").css("display","block");
-		$(".help1 span").text("×");
-		$(".help1").css("font-weight","bolder");
-	}
-	if (help =="×"){
-		$(".display1").css("display","none");
-		$(".help1 span").text("+");
-		$(".help1").css("font-weight","");
-	}
-		
-}
-function help2() {
-	var help = $(".help2 span").text();
-	if (help =="+") {
-		$(".display2").css("display","block");
-		$(".help2 span").text("×");
-		$(".help2").css("font-weight","bolder");
-	}
-	if (help =="×"){
-		$(".display2").css("display","none");
-		$(".help2 span").text("+");
-		$(".help2").css("font-weight","");
-	}
-		
-}
-function help3() {
-	var help = $(".help3 span").text();
-	if (help =="+") {
-		$(".display3").css("display","block");
-		$(".help3 span").text("×");
-		$(".help3").css("font-weight","bolder");
-	}
-	if (help =="×"){
-		$(".display3").css("display","none");
-		$(".help3 span").text("+");
-		$(".help3").css("font-weight","");
-	}
-		
-}
-function help4() {
-	var help = $(".help4 span").text();
-	if (help =="+") {
-		$(".display4").css("display","block");
-		$(".help4 span").text("×");
-		$(".help4").css("font-weight","bolder");
-		$(".help4").removeClass('box_buttom');
-		$(".display4").addClass('box_buttom bottom');
-	}
-	if (help =="×"){
-		$(".display4").css("display","none");
-		$(".help4 span").text("+");
-		$(".help4").css("font-weight","");
-		$(".help4").addClass('box_buttom');
-	}
-		
-}
